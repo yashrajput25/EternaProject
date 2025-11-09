a1bc8d1e4e3c7333c30728295848599a2f5d87d1
Yash Rajput <yashrajputyr10@gmail.com>
2025-11-09 14:06:07 +0530

    register orderRoute alongside pingRoute in Fastify server

--- order-engine/src/index.ts
+++ order-engine/src/index.ts
@@ -1,12 +1,15 @@
 import Fastify from "fastify";
 import websocketPlugin from "@fastify/websocket";
 import pingRoute from "./routes/pingRoute";
+import orderRoute from "./routes/orderRoute";
 
 const server = Fastify({ logger: true });
 server.register(websocketPlugin);
 
 // test route
 server.register(pingRoute, { prefix: "/api" });
+server.register(orderRoute, { prefix: "/api" });
+
 
 // start server
 const start = async () => {
