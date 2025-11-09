a3e29f3fab2807c948ff9668988d5ecc927e632c
Yash Rajput <yashrajputyr10@gmail.com>
2025-11-09 17:03:17 +0530

    initialize PostgreSQL and order queue during Fastify server startup

--- order-engine/src/index.ts
+++ order-engine/src/index.ts
@@ -2,6 +2,8 @@ import Fastify from "fastify";
 import websocketPlugin from "@fastify/websocket";
 import pingRoute from "./routes/pingRoute";
 import orderRoute from "./routes/orderRoute";
+import { initDB } from "./config/db";
+import { orderQueue } from "./services/OrderQueue"; 
 
 const server = Fastify({ logger: true });
 server.register(websocketPlugin);
@@ -13,13 +15,19 @@ server.register(orderRoute, { prefix: "/api" });
 
 // start server
 const start = async () => {
-  try {
-    await server.listen({ port: 3000 });
-    console.log("✅ Server running on http://localhost:3000");
-  } catch (err) {
-    server.log.error(err);
-    process.exit(1);
-  }
+    try {
+        await initDB();
+        console.log("✅ PostgreSQL connected successfully");
+    
+        // ✅ Initialize the order queue only *after* DB is ready
+        orderQueue; 
+    
+        await server.listen({ port: 3000, host: "0.0.0.0" });
+        console.log("✅ Server running on http://localhost:3000");
+        } catch (err) {
+        server.log.error(err);
+        process.exit(1);
+    }
 };
 
 start();
