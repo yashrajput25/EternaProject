5d874fc5a2f219691231d997e74059f987ac524c
Yash Rajput <yashrajputyr10@gmail.com>
2025-11-09 18:23:10 +0530

    dependencies

---
+++ order-engine/jest.config.js
@@ -0,0 +1,14 @@
+const { createDefaultPreset } = require("ts-jest");
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ["<rootDir>/tests"],
+  moduleFileExtensions: ["ts", "js"],
+  clearMocks: true,
+  verbose: true,
+  forceExit: true
+};
